4b133b9e966b4e4b0cbf6862248f766f1db95220
bellescoincidences <68645408+bellescoincidences@users.noreply.github.com>
2026-07-01 12:26:05 +0100

    build: add data_reader role into modules/iam-centralised

--- infrastructure/terraform/modules/iam-centralised/outputs.tf
+++ infrastructure/terraform/modules/iam-centralised/outputs.tf
@@ -29,3 +29,13 @@ output "team_role_arns" {
   description = "Map of team role names to their ARNs."
   value       = { for k, v in aws_iam_role.team : k => v.arn }
 }
+
+# --------------------------------------------------------------------------
+# Data reader role for synthetic data engine
+# --------------------------------------------------------------------------
+
+
+output "data_reader_role_arn" {
+  description = "ARN of the scoped data-reader role, if created. This is the ROLE_ARN given to internal and cross-government consumers."
+  value       = var.create_data_reader_role ? aws_iam_role.data_reader[0].arn : null
+}
